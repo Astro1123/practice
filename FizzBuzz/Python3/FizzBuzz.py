fb98cd4da630abb5136de5e627f46b24fa729226
Astro1123 <adansonia_digitata_6674@outlook.jp>
2021-07-05 01:20:00 +0900

    first commit

---
+++ FizzBuzz/Python3/FizzBuzz.py
@@ -0,0 +1,15 @@
+print('n = ', end = '')
+n = int(input())
+if n < 1:
+	n = 100
+print('')
+for i in range(n):
+	if (i+1)%15 == 0:
+		print('Fizz Buzz')
+	elif (i+1)%5 == 0:
+		print('Buzz')
+	elif (i+1)%3 == 0:
+		print('Fizz')
+	else:
+		print(i+1)
+print('')
