ab9b40985852cbcaecd4d88188c15a1ef82d920f
Astro1123 <adansonia_digitata_6674@outlook.jp>
2021-07-11 20:36:57 +0900

    first commit

--- Prime/Perl/Prime.pl
+++ Prime/Perl/Prime.pl
@@ -1,16 +1,28 @@
 sub Prime {
-	if ($_[0] < 2) {return -1;}
-	if ($_[0] == 2) {return 1;}
-	if ($_[0]%2 == 0) {return 0;}
+	if ($_[0] < 2) {
+		return -1;
+	}
+	if ($_[0] == 2) {
+		return 1;
+	}
+	if ($_[0]%2 == 0) {
+		return 0;
+	}
 	for ($i = 3; $i <= sqrt($_[0]); $i+=2) {
-		if ($_[0]%$i == 0) {return 0;}
+		if ($_[0]%$i == 0) {
+			return 0;
+		}
 	}
 	1;
 }
 
 sub PrintPrime {
-	if ($_[0] < 0) {return "Error";}
-	if ($_[0] == 0) {return "Composite number";}
+	if ($_[0] < 0) {
+		return "Error";
+	}
+	if ($_[0] == 0) {
+		return "Composite number";
+	}
 	"Prime number";
 }
 
@@ -24,5 +36,7 @@ while(1) {
 	print("Continue? (Yes : 1 / No : 0) : ");
 	my $i = <STDIN>;
 	print "\n";
-	if($i == 0) {last;}
+	if($i == 0) {
+		last;
+	}
 }
