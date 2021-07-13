3a545d4cd888695b57da04c596a2a458d76e34da
Astro1123 <adansonia_digitata_6674@outlook.jp>
2021-07-13 19:46:50 +0900

    first commit

---
+++ FizzBuzz/CSharp/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public static void Main()
+        {
+            Console.Write("n = ");
+            var line1 = Console.ReadLine().Trim();
+            var n = int.Parse(line1);
+            n = (n < 1 ? 100 : n);
+            Console.WriteLine("");
+            for (var i = 0; i < n; i++ ) {
+                if ((i+1) % 15 == 0) {
+                    Console.WriteLine("Fizz Buzz");
+                } else if ((i + 1) % 5 == 0) {
+                    Console.WriteLine("Buzz");
+                } else if ((i + 1) % 3 == 0) {
+                    Console.WriteLine("Fizz");
+                } else {
+                    Console.WriteLine(i+1);
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
