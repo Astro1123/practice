ccb7fab57f97473ce29c71bada559910f4af07ef
Astro1123 <adansonia_digitata_6674@outlook.jp>
2021-07-13 20:02:53 +0900

    first commit

---
+++ Fibonacci/CSharp/Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fibonacci {
+    public class Fib {
+        public static void Main() {
+            Console.Write("n = ");
+            var line1 = Console.ReadLine().Trim();
+            var n = int.Parse(line1);
+            n = (n < 0 ? 0 : n);
+            Console.WriteLine("");
+            for (var i = 0; i <= n; i++) {
+                Console.WriteLine("{0} : {1}",i,Fibonacci(i));
+            }
+            Console.WriteLine("");
+        }
+
+        static int Fibonacci(int x) {
+            return (x < 2 ? x : Fibonacci(x - 1) + Fibonacci(x - 2));
+        }
+    }
+}
