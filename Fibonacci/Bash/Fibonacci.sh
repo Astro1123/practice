294686ca96db1ad81d2fe170c166e38dda60c735
Astro1123 <adansonia_digitata_6674@outlook.jp>
2021-10-07 17:36:40 +0900

    first commit

---
+++ Fibonacci/Bash/Fibonacci.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo -n "n = "
+read n
+if [ $n -lt 0 ]
+then
+	n=0
+fi
+
+echo ""
+
+k=0
+l=1
+j=1
+
+if [ $n -lt 2 ]
+then 
+	for ((i=0 ; i<n+1 ; i++))
+	do
+		echo "$i : 1" 
+	done
+else
+	for ((i=0 ; i<n+1 ; i++))
+	do
+		echo "$i : $j"
+		j=$(($k+$l))
+		k=$l
+		l=$j
+	done
+fi
+echo ""
