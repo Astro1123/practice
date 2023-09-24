f73f0531fa37443d2ae4e2c9f96e7e4f76912f0e
Astro1123 <adansonia_digitata_6674@outlook.jp>
2023-09-25 00:11:23 +0900

    first commit

--- Sort/Java/src/Test/TestDriver.java
+++ Sort/Java/src/Test/TestDriver.java
@@ -1,44 +1,33 @@
 package Test;
 
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.Iterator;
+
 import Sort.*;
 import Sort.Const.*;
-import Sort.BubbleSort.*;
-import Sort.GnomeSort.*;
-import Sort.MergeSort.*;
-import Sort.QuickSort.*;
-import Sort.SelectionSort.*;
-import Sort.InsertionSort.*;
-import Sort.HeapSort.*;
-import Sort.ShakerSort.*;
 
 public class TestDriver {
 	private final int LENGTH = 16;
+	private final String[] LIST = {"Bubble", "Selection", "Insertion", "Heap", "Merge", "Quick"};
 	
 	public static void main(String[] args) {
 		TestDriver td = new TestDriver();
-		System.out.println("Bubble sort:");
-		td.test(new BubbleSort());
+		td.startDriver();
+	}
+	
+	private void startDriver() {
+		SortMap sm = new SortMap();
+		sm.makeMap();
+		Map<String, Sort> sortMap = sm.getSortMap(LIST);
+		
 		System.out.println("----------------------------------------");
-		System.out.println("Gnome sort:");
-		td.test(new GnomeSort());
-		System.out.println("----------------------------------------");
-		System.out.println("Merge sort:");
-		td.test(new MergeSort());
-		System.out.println("----------------------------------------");
-		System.out.println("Quick sort:");
-		td.test(new QuickSort());
-		System.out.println("----------------------------------------");
-		System.out.println("Selection sort:");
-		td.test(new SelectionSort());
-		System.out.println("----------------------------------------");
-		System.out.println("Insertion sort:");
-		td.test(new InsertionSort());
-		System.out.println("----------------------------------------");
-		System.out.println("Heap sort:");
-		td.test(new HeapSort());
-		System.out.println("----------------------------------------");
-		System.out.println("Shaker sort:");
-		td.test(new ShakerSort());
+		for (Iterator<String> itr = sortMap.keySet().iterator(); itr.hasNext();) {
+			String key = itr.next();
+			System.out.println(String.format("%s sort:", key));
+			test(sortMap.get(key));
+			System.out.println("----------------------------------------");
+		}
 	}
 	
 	private void test(Sort sort) {
